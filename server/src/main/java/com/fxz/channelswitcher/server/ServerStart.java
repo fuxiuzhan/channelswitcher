1819e9c6d50379b49b8dbc7b4a77ad112293cfbc
admin <fuxiuzhan>
2020-05-19 23:25:52 +0800

    stru

---
+++ server/src/main/java/com/fxz/channelswitcher/server/ServerStart.java
@@ -0,0 +1,15 @@
+package com.fxz.channelswitcher.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fxz
+ */
+@SpringBootApplication
+public class ServerStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStart.class, args);
+    }
+}
